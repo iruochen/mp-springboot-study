187eba681eaae6bf53772180d8436f5c8f0ff6fe
ruochen <wangrui@ruochen.email>
2022-03-15 11:17:51 +0800

    springboot整合mp

---
+++ src/main/java/com/ruochen/mp/MyApplication.java
@@ -0,0 +1,13 @@
+package com.ruochen.mp;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.ruochen.mp.mapper") //设置mapper接口的扫描包
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
---
+++ src/main/java/com/ruochen/mp/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.ruochen.mp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ruochen.mp.pojo.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
