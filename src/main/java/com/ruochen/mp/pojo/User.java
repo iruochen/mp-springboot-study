874745313b6d2271cb35e9bdb2c233503454e2d5
ruochen <wangrui@ruochen.email>
2022-03-15 11:36:37 +0800

    tableField注解详解

--- src/main/java/com/ruochen/mp/pojo/User.java
+++ src/main/java/com/ruochen/mp/pojo/User.java
@@ -1,6 +1,7 @@
 package com.ruochen.mp.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
@@ -17,8 +18,15 @@ public class User {
     @TableId(type = IdType.AUTO)
     private Long id;
     private String userName;
+
+    @TableField(select = false)  // 查询时不返回该字段的值
     private String password;
     private String name;
     private Integer age;
-    private String email;
+
+    @TableField(value = "email")  // 指定数据库表中字段名
+    private String mail;
+
+    @TableField(exist = false)  // 表示此字段在数据库表中不存在
+    private String address; // 在数据库表中不存在
 }
--- src/test/java/com/ruochen/mp/TestUserMapper.java
+++ src/test/java/com/ruochen/mp/TestUserMapper.java
@@ -17,10 +17,10 @@ public class TestUserMapper {
     @Test
     public void testInsert() {
         User user = new User();
-        user.setEmail("1@2.com");
-        user.setAge(12);
-        user.setUserName("caocao");
-        user.setName("曹操");
+        user.setMail("2@2.com");
+        user.setAge(23);
+        user.setUserName("caopi");
+        user.setName("曹丕");
         user.setPassword("123456");
 
         int result = this.userMapper.insert(user);  // result：数据库受影响的行数
@@ -28,5 +28,11 @@ public class TestUserMapper {
 
         // 获取自增长后id值，自增长后的id值会回填到user对象中
         System.out.println("id => " + user.getId());
+    }
+
+    @Test
+    public void testSelectById() {
+        User user = this.userMapper.selectById(1L);
+        System.out.println(user);
     }
 }
